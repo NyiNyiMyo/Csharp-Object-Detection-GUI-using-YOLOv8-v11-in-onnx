c7f43779c2486843ce6606e0abd10926b402414c
Nyi-Nyi-Myo <nyinyi.m@kkumail.com>
2025-10-26 14:23:28 +0700

    codes uploaded

---
+++ gui_detection_onnx/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace gui_detection_onnx
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(449, 988);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Load Image";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaption;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(99, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1730, 924);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(692, 989);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 60);
+            button2.TabIndex = 2;
+            button2.Text = "Run Inference";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonShadow;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.ForeColor = SystemColors.ControlLightLight;
+            button3.Location = new Point(1115, 988);
+            button3.Name = "button3";
+            button3.Size = new Size(200, 60);
+            button3.TabIndex = 3;
+            button3.Text = "Load Folder";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1363, 989);
+            button4.Name = "button4";
+            button4.Size = new Size(240, 60);
+            button4.TabIndex = 4;
+            button4.Text = "Run Grid Inference";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Makers - YOLOv8 Object Detection Onnx";
+            this.Resize += Form1_Resize;
+            this.Shown += Form1_Shown;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
